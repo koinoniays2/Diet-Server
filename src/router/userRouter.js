24b68835e093e45ea7484958fcd0bc118df7a01b
koinonia2 <koinonia_ys@naver.com>
2024-12-13 16:41:39 +0900

    로그아웃 post 스정

--- src/router/userRouter.js
+++ src/router/userRouter.js
@@ -7,7 +7,7 @@ userRouter.post("/create", createUser);
 userRouter.post("/login", login);
 
 userRouter.get("/login-status", loginStatus); // 로그인 여부
-userRouter.get("/logout", logout); // 로그아웃
+userRouter.post("/logout", logout); // 로그아웃
 
 
 export default userRouter;
